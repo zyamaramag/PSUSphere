cc16dd561177406d99a4d3fe837b6f5907d1b1be
zyamaramag <mgadielzya@gmail.com>
2026-03-10 23:53:03 +0800

    fixed folders

---
+++ projectsite/studentorg/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
