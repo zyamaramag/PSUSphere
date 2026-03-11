2fa5730985a664e6f6e565afd68eb107d393d17e
zyamaramag <mgadielzya@gmail.com>
2026-03-11 23:08:46 +0800

    modified urls.py, views.py, and org_list.html then added forms.py and org_form.html

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Organization
@@ -12,3 +15,9 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list')
