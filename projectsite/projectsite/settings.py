972fa31100fe07bdfa14c0a602025a90e35bddf6
Charles <charlesdarwinduerme@gmail.com>
2026-03-21 16:19:54 +0800

    Updated Configuration on settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -29,7 +29,18 @@ DEBUG = True
 ALLOWED_HOSTS = ['psuspherelc.pythonanywhere.com', '127.0.0.1']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+LOGIN_URL = '/accounts/login/' # where @login_required will send users
+LOGIN_REDIRECT_URL = '/' # where to go after successful login
+LOGOUT_REDIRECT_URL = '/accounts/login/' # after logout, go back to login
+ACCOUNT_LOGOUT_REDIRECT_URL = '/' # where to redirect after logout
+ACCOUNT_LOGOUT_ON_GET = True # logout immediately on GET
+ACCOUNT_LOGIN_METHODS = {"username", "email"} # allow login with username OR email
+ACCOUNT_SIGNUP_FIELDS = [
+"username*",
+"email*",
+"password1*",
+"password2*",
+]
 # Application definition
 
 LOGIN_URL = '/accounts/login/' # where @login_required will send users
